0223b5e9ce4b45cdabb7b22b6f36f70a354c85fa
anandshuklaweb <anandshukla.web@gmail.com>
2022-08-22 10:08:28 +0530

    real estate app

---
+++ components/Footer.jsx
@@ -0,0 +1,19 @@
+import { Box } from "@chakra-ui/react";
+
+const Footer = () => {
+  return (
+    <Box
+      textAlign="center"
+      p="5"
+      color="red.600"
+      borderTop="1px"
+      borderColor="#ff3c3c"
+      marginTop="8"
+      fontWeight="medium"
+    >
+      2022, Real Estate.
+    </Box>
+  );
+};
+
+export default Footer;
